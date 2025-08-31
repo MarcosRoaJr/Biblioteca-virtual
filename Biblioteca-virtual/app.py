175ffb5b700ba5fc016f70884d4bf760c89f26f2
MarcosRoaJr <marcosroajr@gmail.com>
2025-08-31 19:35:23 -0300

    Add files via upload

--- Biblioteca-virtual/app.py
+++ Biblioteca-virtual/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import random
-from bd import init_app, verificar_usuario
+import bd
+
+# Retornar_x eu uso para buscar informações para usar em outra função que realmente vai buscar a informação que eu preciso
+# Buscar_x eu uso para enviar a informação que eu vou buscar de fato no banco e usar
 
 app = Flask(__name__)
 app.secret_key = str(random.randint(0, 10))
 
-init_app(app)
+bd.init_app(app)
 
 # Rota para pagina inicial
 @app.route("/home", methods=["GET", "POST"])
@@ -21,12 +24,92 @@ def cadastro():
 # Rota para buscar as informações de livros
 @app.route("/buscar", methods=["GET", "POST"])
 def buscar():
-    return render_template("rotas/buscar.html", show_navbar=True)
+    livro = None
+    titulos = bd.buscar_livro_nome()
+    genero = None
+    editora = None
+    campus = None
+    locallivro = None
+
+    if request.method == "POST":
+        isbnlivro = (request.form.get("isbn_livro") or "").strip()
+        idlivro = (request.form.get("identificador_livro") or "").strip()
+        escolhido = (request.form.get("livro_escolhido") or "").strip()
+
+        if escolhido:
+            if escolhido.isdigit():
+                livro = bd.buscar_livro_id(int(escolhido))
+                locallivro = bd.buscar_locallivro(int(escolhido))
+                idgenero = bd.retornar_idgenero(int(escolhido))
+                ideditora = bd.retornar_ideditora(int(escolhido))
+
+                genero = bd.buscar_genero(idgenero)
+                editora = bd.buscar_editora(ideditora)
+                if locallivro:
+                    campus = bd.buscar_campus(int(locallivro["id_campus"]))
+                else:
+                    campus = None
+        elif idlivro:
+            if idlivro.isdigit():
+                livro = bd.buscar_livro_id(int(idlivro))
+                idgenero = bd.retornar_idgenero(int(idlivro))
+                ideditora = bd.retornar_ideditora(int(idlivro))
+                locallivro = bd.buscar_locallivro(int(idlivro))
+
+                genero = bd.buscar_genero(idgenero)
+                editora = bd.buscar_editora(ideditora)
+                if locallivro:
+                    campus = bd.buscar_campus(int(locallivro["id_campus"]))
+                else:
+                    campus = None
+        elif isbnlivro:
+            if isbnlivro.isdigit():
+                livro = bd.buscar_livro_isbn(int(isbnlivro))
+                isbn_idlivro = bd.retornar_idisbn(int(isbnlivro))
+
+                if isbn_idlivro is not None:
+                    r = bd.retornar_idgenero(isbn_idlivro)
+                    e = bd.retornar_ideditora(isbn_idlivro)
+                    t = bd.buscar_locallivro(isbn_idlivro)
+                    o = bd.buscar_campus(isbn_idlivro)
+                    if r is not None:
+                        genero = bd.buscar_genero(r)
+                        editora = bd.buscar_editora(e)
+                        locallivro = t
+                        campus = o
+
+    return render_template("rotas/buscar.html", show_navbar=True, campus=campus, locallivro=locallivro, editora=editora,livro=livro,titulos=titulos, genero=genero)
+
 
 # Rota para cadastrar o usuario
+# nome, data_nascimento, email, cpf, telefone, senha, nivel_acesso
 @app.route("/cadastrar_usuario", methods=["GET", "POST"])
 def cadastrar_usuario():
-    return render_template("rotas/cadastrar_usuario.html", show_navbar=True)
+    confirma = None
+    if request.method == "POST":
+        nome_info = request.form.get("Nome")
+        data_nascimento_info = request.form.get("Data_nascimento")
+        email_info = request.form.get("Email")
+        cpf_info = request.form.get("CPF")
+        telefone_info = request.form.get("Telefone")
+        senha_info = request.form.get("Senha")
+        c_senha_info = request.form.get("c_Senha")
+        lvl_acess_info = request.form.get("lvl_acess")
+        validar_confirma = request.form.get("confirmacao")
+        validar_negado = request.form.get("negado")
+        verificar = bd.verificar_usuario(email_info,cpf_info)
+        
+        if validar_confirma == "confirmar":
+            if not verificar:
+                if senha_info == c_senha_info:
+                    bd.criar_usuario(nome_info, data_nascimento_info, email_info, cpf_info, telefone_info, senha_info, lvl_acess_info)
+                    confirma = True
+                else:
+                    confirma = False
+        elif validar_negado == "negar":
+            confirma = False
+            
+    return render_template("rotas/cadastrar_usuario.html", show_navbar=True, msg=confirma)
 
 # Pagina para efetuar o login
 @app.route("/", methods=["GET", "POST"])
@@ -35,8 +118,8 @@ def login():
         usuario_id = request.form.get("usuario")
         senha = request.form.get("senha")
 
-        if verificar_usuario(usuario_id, senha) or (usuario_id == "0" and senha == "0"):
-            user = verificar_usuario(usuario_id, senha)
+        if bd.verificar_usuario(usuario_id, senha) or (usuario_id == "0" and senha == "0"):
+            user = bd.verificar_usuario(usuario_id, senha)
             if user:
                 dn = user.get("data_nascimento")
                 dn_fmt = dn.strftime("%d/%m/%Y") if dn else None
@@ -63,7 +146,7 @@ def login():
                     "Nivel-acesso": "admin",
                 }
                 return redirect(url_for('home'))
-        elif not session.get("user") or not verificar_usuario(usuario_id, senha):
+        elif not session.get("user") or not bd.verificar_usuario(usuario_id, senha):
             return render_template("rotas/index.html", erro=True, msg="Usuário ou senha inválidos.")
     return render_template("rotas/index.html")
 
--- Biblioteca-virtual/bd.py
+++ Biblioteca-virtual/bd.py
@@ -2,7 +2,6 @@ from flask_mysqldb import MySQL
 
 mysql = MySQL()
 
-# Configuração do banco de dados para o python
 def init_app(app):
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_PORT'] = 3307
@@ -13,19 +12,129 @@ def init_app(app):
     mysql.init_app(app)
     return mysql
 
+# ==================BUSCAS DADOS=================
+
+# Cara aqui vai procura o id do livro e retorna os generos do livro
+def retornar_idgenero(identificador: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM livro_genero WHERE id_livro = %s;", (identificador,))
+    row = conectar.fetchone()
+    conectar.close()
+    return row["id_genero"] if row else None
+
+# Aqui retorna o id da editora
+def retornar_ideditora(identificador: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM livro WHERE id_livro = %s;", (identificador,))
+    row = conectar.fetchone()
+    conectar.close()
+    return row["id_editora"] if row else None
+
+def buscar_editora(identificador: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM editora WHERE id_editora = %s;", (identificador,))
+    row = conectar.fetchone()
+    conectar.close()
+    return row["Nome"] if row else None
+
+def retornar_idisbn(identificador: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM livro WHERE ISBN = %s;", (identificador,))
+    row = conectar.fetchone()
+    conectar.close()
+    return row["id_livro"] if row else None
+
+def buscar_genero(identificador: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM genero WHERE id_genero = %s;", (identificador,))
+    row = conectar.fetchone()
+    conectar.close()
+    return row["genero"] if row else None
+
+def formatar_isbn(isbn: str) -> str:
+    if not isbn:
+        return isbn
+    # garante que é string
+    isbn = str(isbn)
+    return f"{isbn[0:3]}-{isbn[3:5]}-{isbn[5:10]}-{isbn[10:12]}"
+
 
 # Busca as informações do usuario
 # função em potencial para trocar o nome, ver depois de não é ideal ver a troca dos nomes
 def verificar_usuario(usuario_id, senha):
-    cur = mysql.connection.cursor()
-    cur.execute("""
-        SELECT *
-        FROM usuarios
-        WHERE id_usuario = %s
-    """, (usuario_id,))
-    row = cur.fetchone()
-    cur.close()
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM usuarios WHERE id_usuario = %s; ", (usuario_id,))
+    listar = conectar.fetchone()
+    conectar.close()
 
-    if not row:
+    if not listar:
         return None
-    return row if row['senha'] == senha else None
+    return listar if listar['senha'] == senha else None
+
+# Busca os livros por ID
+def buscar_livro_id(id_livro: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM livro WHERE id_livro = %s;", (id_livro,))
+    listar = conectar.fetchone()
+    conectar.close()
+
+    if listar and listar.get("ISBN"):
+        listar["ISBN"] = formatar_isbn(listar["ISBN"])
+    return listar
+
+# Aqui ele busca livros pelo nome junto com o select
+def buscar_livro_nome():
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM livro ORDER BY titulo;")
+    listar_lvr = conectar.fetchall()
+    conectar.close()
+
+    for r in listar_lvr:
+        if r.get("ISBN"):
+            r["ISBN"] = formatar_isbn(r["ISBN"])
+    return listar_lvr
+
+def buscar_livro_isbn(ISBN: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM livro WHERE ISBN = %s;", (ISBN,))
+    listar = conectar.fetchone()
+    conectar.close()
+
+    if listar and listar.get("ISBN"):
+        listar["ISBN"] = formatar_isbn(listar["ISBN"])
+    return listar
+
+# chamar a tabela toda da localidade do livro depois achar o câmpus que ele está com outra função
+
+def buscar_locallivro(id_livro: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM posicao_livro WHERE id_livro = %s;", (id_livro,))
+    listar = conectar.fetchone()
+    conectar.close()
+
+    return listar
+
+def buscar_campus(id_capus: int):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT * FROM localidade_livro WHERE id_campus = %s", (id_capus,))
+    listar = conectar.fetchone()
+    conectar.close()
+
+    return listar
+
+
+# =================Inserir dados=================
+
+def criar_usuario(nome, data_nascimento, email, cpf, telefone, senha, nivel_acesso):
+    conectar = mysql.connection.cursor()
+    conectar.execute("INSERT INTO usuarios (nome, data_nascimento, email, cpf, telefone, senha, nivel_acesso) VALUES (%s, %s, %s , %s, %s, %s, %s)", (nome, data_nascimento, email, cpf, telefone, senha, nivel_acesso,)) 
+    mysql.connection.commit()
+    conectar.close()
+
+def validar_usuario(email_info,cpf_info):
+    conectar = mysql.connection.cursor()
+    conectar.execute("SELECT email, cpf FROM usuarios WHERE email = %s OR cpf = %s", (email_info, cpf_info,))
+    existe = conectar.fetchone()
+    conectar.close()
+
+    return existe
